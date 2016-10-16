ea72084349b88a8a1915e58464e71a96580e6eec
evgenyb <belki.sky@gmail.com>
2016-10-16 15:39:01 +0300

    run.bat runs all together now

--- run.bat
+++ run.bat
@@ -1,4 +1,8 @@
+set SCRIPT_DIR=%~dp0
+pushd %SCRIPT_DIR%
 
+start %SCRIPT_DIR%\tools\pocketsphinx\pocketsphinx_continuous_RU.bat 
+	
 python src\voiceRecorder.py svm
 ::python src\voiceRecorder.py knn
 ::python src\voiceRecorder.py randomforest
